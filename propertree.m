36a4fc9d5fff08e2a23dde27c77aaf7e6c2a2b5e
vaibhav2010 <vaibhavkrishna2010@gmail.com>
2017-11-30 22:00:06 +0530

    Usual commit

--- propertree.m
+++ propertree.m
@@ -3,9 +3,11 @@ Z=input('Enter Z matrix');
 s=[];
 t=[];
 weights=[];
+Bno=[];
 for x=1:size(A,2)
     s=[s find(A(:,x)==1)];
     t=[t find(A(:,x)==-1)];
+    Bno=[Bno x];
     if(Z(x,x)==conj(Z(x,x)))
         weights=[weights 0];
     else
@@ -15,12 +17,16 @@ for x=1:size(A,2)
             weights=[weights 1];
         end;
     end;
-end;
-G=graph(s,t,weights);
-G.Edges.Bno=[1:size(A,2)]';
+end
+S.EndNodes=[s' t'];
+S.Weight=weights'; 
+S.Bno=Bno';
+Edges=struct2table(S);
+G=graph(Edges);
 p = plot(G,'EdgeLabel',G.Edges.Weight,'Layout','layered');
 T=minspantree(G,'Method','sparse');
 highlight(p,T);
+A
 Fmat;
 
     
